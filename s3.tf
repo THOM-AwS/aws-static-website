b4a9494f65eac90142d58d783fc4b00bf8c211f8
Thomas Hamer <48810100+THOM-AwS@users.noreply.github.com>
2021-09-14 22:55:05 +1000

    WIP: add S3 config

---
+++ s3.tf
@@ -0,0 +1,63 @@
+resource "aws_s3_bucket" "root" {
+  bucket = lower(var.bucket_name)
+  acl    = "private"
+  tags = var.tags
+}
+
+resource "aws_s3_bucket" "www" {
+  bucket = lower(join(",", ["www.",var.bucket_name]))
+  acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  dynamic "logging" {
+    for_each = length(keys(var.logging)) == 0 ? [] : [var.logging]
+
+    content {
+      target_bucket = logging.value.target_bucket
+      target_prefix = lookup(logging.value, "target_prefix")
+    }
+  }
+  dynamic "bucket_policy" {
+    for_each = length(keys(var.attach_bucket_policy)) == 0 ? [] : [var.policy_document]
+
+    content {
+      target_bucket = logging.value.target_bucket
+      target_prefix = lookup(logging.value, "target_prefix")
+    }
+  }
+  versioning {
+    enabled = var.versioning_status
+  }
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.www.id
+
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
+
+resource "aws_s3_bucket" "log_bucket" {
+  count = var.create_logging_bucket ? 1 : 0
+  bucket = lower(join(",", [var.bucket_name,"-logging"]))
+  acl    = "log-delivery-write"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  tags = var.tags
+}
+
