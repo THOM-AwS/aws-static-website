299ab053d9ffbdb04b19bd6ce03ce632cd42f0ab
Thomas Hamer <thomas_hamer@outlook.com>
2021-09-18 22:01:17 +1000

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
-# output "bucket_id" {
-#   description = "The name of the bucket"
-#   value       = module.this.aws_s3_bucket.www.id
-# }
+output "bucket_id" {
+  description = "The name of the bucket"
+  value       = aws_s3_bucket.www.id
+}
 
-# output "bucket_arn" {
-#   description = "The ARN of the bucket"
-#   value       = module.this.aws_s3_bucket.www.arn
-# }
+output "bucket_arn" {
+  description = "The ARN of the bucket"
+  value       = aws_s3_bucket.www.arn
+}
 
-# output "user_details" {
-#   description = "User Name"
-#   value       = module.this.aws_iam_user.website-user.name
-# }
+output "user_details" {
+  description = "User Name"
+  value       = aws_iam_user.website-user.name
+}
 
-# output "user_acces_key" {
-#   description = "User Name"
-#   value       = module.this.aws_iam_access_key.website-user.id
-# }
+output "user_acces_key" {
+  description = "User Name"
+  value       = aws_iam_access_key.website-user.id
+}
 
-# output "user_secret_access_key" {
-#   description = "User Name"
-#   value       = module.this.aws_iam_access_key.website-user.secret
-# }
+output "user_secret_access_key" {
+  description = "User Name"
+  value       = aws_iam_access_key.website-user.secret
+}
