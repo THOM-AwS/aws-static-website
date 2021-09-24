569942c68a7f55e680f295e9116208bac5987047
Thomas Hamer <thomas_hamer@outlook.com>
2021-09-24 22:26:55 +1000

    tostring 2nd try

--- iam.tf
+++ iam.tf
@@ -52,7 +52,7 @@ EOF
 
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name                  = "website-${tostring(replace(var.domain_name, ".", "-"))}-execution_role"
+  name                  = tostring(website-${replace(var.domain_name, ".", "-")}-execution_role)
   force_detach_policies = true
 
   inline_policy {
