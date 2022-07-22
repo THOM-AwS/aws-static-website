dcd3eb6bbc1a2c91edd8e7bfea79d7e3ff4460fc
Thomas Hamer <thomas_hamer@outlook.com>
2022-07-22 23:31:49 +1000

    update to allow ip lookup url

--- secheader.py
+++ secheader.py
@@ -12,7 +12,7 @@ STATIC_HEADERS_TO_ADD = {
         "default-src 'self';" 
         "base-uri 'self';"
         "img-src * 'self' data: https: 'unsafe-inline';" 
-        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://maps.gstatic.com http://ipinfo.io/;"
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.gstatic.com *.googleapis.com https://ipinfo.io;"
         "style-src 'self' 'unsafe-inline' *.googleapis.com data:;"
         "font-src 'self' 'unsafe-inline' *.gstatic.com *.googleapis.com;" 
         "frame-src youtube.com www.youtube.com;" 
