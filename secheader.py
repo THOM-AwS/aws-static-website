276824526f795c514a85a1ab4def8ef90db5d0df
Thomas Hamer <thomas_hamer@outlook.com>
2022-07-26 22:17:22 +1000

    update CSP

--- secheader.py
+++ secheader.py
@@ -12,7 +12,7 @@ STATIC_HEADERS_TO_ADD = {
         "default-src 'self' data: https://geoip-js.com *.googleapis.com;" 
         "base-uri 'self';"
         "img-src * 'self' data: https: 'unsafe-inline';" 
-        "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.googleapis.com https://maps.gstatic.com https://www.youtube.com *.google.com https://*.gstatic.com data: blob:;"
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.googleapis.com https://maps.gstatic.com https://www.youtube.com *.google.com https://*.gstatic.com https://geoip-js.com data: blob:;"
         "style-src 'self' 'unsafe-inline' *.googleapis.com https://fonts.googleapis.com data:;"
         "font-src 'self' 'unsafe-inline' *.gstatic.com *.googleapis.com;" 
         "frame-src https://youtube.com https://www.youtube.com *.google.com;" 
