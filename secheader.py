44010b95b5bcffa9465afcb9df53c47f112f8edf
Thomas Hamer <thomas_hamer@outlook.com>
2022-07-26 20:31:00 +1000

    update for geo loc

--- secheader.py
+++ secheader.py
@@ -12,7 +12,7 @@ STATIC_HEADERS_TO_ADD = {
         "default-src 'self';" 
         "base-uri 'self';"
         "img-src * 'self' data: https: 'unsafe-inline';" 
-        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.gstatic.com https://maps.googleapis.com https://geoip-js.com;"
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.gstatic.com https://maps.googleapis.com https://geoip-js.com https://maxmind.com;"
         "style-src 'self' 'unsafe-inline' *.googleapis.com data:;"
         "font-src 'self' 'unsafe-inline' *.gstatic.com *.googleapis.com;" 
         "frame-src youtube.com www.youtube.com;" 
