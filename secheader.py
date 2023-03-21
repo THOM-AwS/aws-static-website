3bc01cafea09a12de8481a7939560bc49f2b10f8
Thomas Hamer <thomas_hamer@outlook.com>
2023-03-21 23:37:18 +1100

    update csp

--- secheader.py
+++ secheader.py
@@ -16,7 +16,7 @@ STATIC_HEADERS_TO_ADD = {
         "style-src 'self' 'unsafe-inline' *.googleapis.com https://fonts.googleapis.com data:;"
         "font-src 'self' 'unsafe-inline' *.gstatic.com *.googleapis.com;"
         "frame-src https://youtube.com https://www.youtube.com *.google.com;"
-        "connect-src 'self' https://www.google-analytics.com;"
+        "connect-src 'self' https://www.google-analytics.com https://maps.googleapis.com;"
         "object-src 'none'"
     }],
     'strict-transport-security': [{
