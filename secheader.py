32ccfd191220359c500cf32bbcdd1d11cc0c73d1
Thomas Hamer <thomas_hamer@outlook.com>
2023-11-17 18:00:58 +0800

    update api

--- secheader.py
+++ secheader.py
@@ -16,7 +16,7 @@ STATIC_HEADERS_TO_ADD = {
         "style-src 'self' 'unsafe-inline' *.googleapis.com https://fonts.googleapis.com data:;"
         "font-src 'self' 'unsafe-inline' *.gstatic.com *.googleapis.com;"
         "frame-src https://youtube.com https://www.youtube.com *.google.com;"
-        "connect-src 'self' https://www.google-analytics.com https://maps.googleapis.com;"
+        "connect-src 'self' https://www.google-analytics.com https://maps.googleapis.com https://api.hamer.cloud;"
         "object-src 'none'"
     }],
     'strict-transport-security': [{
