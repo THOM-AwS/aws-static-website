a527c0c5398b30d1aacd15ef17b50bf5103c4bd1
Thomas Hamer <thomas_hamer@outlook.com>
2023-11-17 21:19:19 +0800

    secheader.py

--- secheader.py
+++ secheader.py
@@ -16,7 +16,7 @@ STATIC_HEADERS_TO_ADD = {
         "style-src 'self' 'unsafe-inline' *.googleapis.com https://fonts.googleapis.com data:;"
         "font-src 'self' 'unsafe-inline' *.gstatic.com *.googleapis.com;"
         "frame-src https://youtube.com https://www.youtube.com *.google.com;"
-        "connect-src 'self' https://www.google-analytics.com https://maps.googleapis.com https://api.hamer.cloud;"
+        "connect-src 'self' https://www.google-analytics.com https://maps.googleapis.com https://api.hamer.cloud https://analytics.google.com;"
         "object-src 'none'"
     }],
     'strict-transport-security': [{
